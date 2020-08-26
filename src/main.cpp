2bf67398371f3ee79b37362df0dd46237538980a
Hossein <hosseinabar42@gmail.com>
2020-08-26 17:22:18 +0430

    nothing

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,17 @@
 #include <iostream>
 #include <string>
 
+#define REDIS_IP "127.0.0.1"
+#define REDIS_PORT 6379
 
 int main(int argc, char** argv)
 {
-  	/// A simple test. 
-    PseudoUnorderedMap<std::string, std::string> p("/var/run/redis/redis.sock", SHARED_MEMORY);
+    /// A simple test. 
+    PseudoUnorderedMap<std::string, std::string> p(REDIS_IP, REDIS_PORT);
 
     p.insert("Hossein","Nazari");
     std::cout << p.at("Hossein") << std::endl;
-   
+
+
    return 0;
 }
